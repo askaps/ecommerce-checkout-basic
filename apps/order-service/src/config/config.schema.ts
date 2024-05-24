6f1c24f6b905701bdf7e5576ce957f52d0805730
Abhishek Pratap Singh <apsingh.algr@gmail.com>
2024-05-24 18:21:14 +0530

    Change default order dicsount nth value to 2

--- apps/order-service/src/config/config.schema.ts
+++ apps/order-service/src/config/config.schema.ts
@@ -5,6 +5,6 @@ export default Joi.object({
   NODE_ENV: Joi.string().valid('dev', 'prod', 'local').default('local'),
   ORDER_SERVICE_APPNAME: Joi.string().default('order-service'),
   ORDER_SERVICE_PORT: Joi.number().default(4000),
-  ORDER_NTH_ORDER_DISCOUNT_COUNT: Joi.number().default(5),
+  ORDER_NTH_ORDER_DISCOUNT_COUNT: Joi.number().default(2),
   ORDER_NTH_ORDER_COUPON_VALUE: Joi.number().default(10),
 });
