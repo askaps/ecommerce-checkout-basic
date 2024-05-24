3564549ba969902571dc9c002a43bf35272e2c9c
Abhishek Pratap Singh <apsingh.algr@gmail.com>
2024-05-24 18:19:53 +0530

    Admin role guard

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -2,3 +2,4 @@ export * from './auth.module';
 export * from './auth.user.interface';
 export * from './req-user.decorator';
 export * from './roles.guard';
+export * from './roles.decorator';
--- libs/auth/src/roles.guard.ts
+++ libs/auth/src/roles.guard.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class RolesGuard extends AuthGuard('bearer') {
+export class RolesGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
   }
@@ -18,33 +18,13 @@ export class RolesGuard extends AuthGuard('bearer') {
    */
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     // Call the super method to check if the request is authenticated via jwt
-    return super.canActivate(context);
-  }
+    super.canActivate(context);
 
-  /**
-   * Handles a request that has been authenticated
-   * This method is called when the request has been successfully authenticated.
-   * @param err - An error that was raised during authentication
-   * @param user - The user object that has been authenticated
-   * @param context - The execution context
-   * @returns The user object if authentication is successful, otherwise throws an exception
-   */
-  handleRequest(err: any, user: any, context: any) {
-    // Throw an exception if there was an error during authentication
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
-
-    // Get the list of roles that are required for this request
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
-
-    // If no roles are specified, return the user object
-    if (!roles) {
-      return user;
-    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
 
     // Check if the user has one of the required roles
-    const authorised: boolean = roles.includes('ADMIN') ? user.isAdmin : true;
+    const authorised: boolean = user.roles?.includes('ADMIN') ?? false;
 
     // If the user does not have one of the required roles, throw an unauthorized exception
     if (!authorised) throw new UnauthorizedException();
